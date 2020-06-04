2b86095e17755f9a8facee5ab3101dde03cd4721
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-04 19:51:00 +0800

    Initial commit

---
+++ src/main/groovy/net/miiingle/qa/automation/web/QaAutomationWebApplication.groovy
@@ -0,0 +1,13 @@
+package net.miiingle.qa.automation.web
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class QaAutomationWebApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(QaAutomationWebApplication, args)
+	}
+
+}
---
+++ src/test/groovy/net/miiingle/qa/automation/web/QaAutomationWebApplicationTests.groovy
@@ -0,0 +1,13 @@
+package net.miiingle.qa.automation.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QaAutomationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
