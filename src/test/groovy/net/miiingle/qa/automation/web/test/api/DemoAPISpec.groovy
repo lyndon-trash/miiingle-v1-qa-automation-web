4a10653246c9dffbea1da90023cea88f65b6127c
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-04 20:12:46 +0800

    Add sample spock test

---
+++ src/test/groovy/net/miiingle/qa/automation/web/test/api/DemoAPISpec.groovy
@@ -0,0 +1,11 @@
+package net.miiingle.qa.automation.web.test.api
+
+import spock.lang.Specification
+
+class DemoAPISpec extends Specification {
+
+    def "should work"() {
+        expect: "1 is 1"
+        1 == 1
+    }
+}
