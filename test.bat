13119f022f28d6241cc5ab75534d988d055f064d
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-07 15:53:14 +0800

    Redirect to docs page after test run

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 call gradlew clean test
 call gradlew allureReport
 call gradlew bootJar -x test
-call start chrome http://localhost:8999/allure/index.html
+call start chrome http://localhost:8999/docs/index.html
 call java -jar build\libs\qa-automation-web-1.0.0.jar
